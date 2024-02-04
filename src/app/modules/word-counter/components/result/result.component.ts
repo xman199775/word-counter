0da35fecad6168029c9ec04cf783c6b9963d6cc1
xman199775 <xman199775@gmail.com>
2024-02-05 00:33:28 +0200

    add commenton important function

--- src/app/modules/word-counter/components/result/result.component.ts
+++ src/app/modules/word-counter/components/result/result.component.ts
@@ -13,7 +13,9 @@ export class ResultComponent {
 
   wordCount(text: string): Array<[string, number]> {
     debugger
+    //remove all redundant white spaces and endlines
     text = text.replace(/(?:\r\n|\r|\n)/g, ' ');
+    //split the words
     const words = text.toLowerCase().split(" ");
     const wordCounts: Map<string, number> = new Map();
     for (const word of words) {
@@ -21,7 +23,9 @@ export class ResultComponent {
       {
         continue;
       }
+      // Get the count of the word if exist
       const count = wordCounts.get(word) || 0;
+      // Add word to the map
       wordCounts.set(word, count + 1);
     }
 
